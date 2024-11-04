193a66fb67c32db931d3fd51c3fff43e5b3260c8
Denis1369 <123146543+Denis1369@users.noreply.github.com>
2024-11-04 20:53:04 +0500

    Add files via upload

---
+++ Model/Apii.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Model;
+
+public partial class Apii
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Sname { get; set; }
+}
